e371734bda892744ec55d760704a29cb74a2aff7
jhonyspicy <jhonyspicy@gmail.com>
2018-08-06 15:17:45 +0900

    add sample loop code

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -8,3 +8,20 @@ console.log(`'Allo 'Allo! Content script s`);
 
 let $hallDedamaActionForm = $('form[name=HallDedamaActionForm]');
 console.log($hallDedamaActionForm);
+
+const list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let promise:Promise<any> = Promise.resolve();
+
+list.forEach((v, i)=> {
+  promise = promise.then(value => new Promise((resolve, reject) => {
+    setTimeout(()=> {
+      console.log(v);
+      resolve();
+    }, Math.random() * 3000);
+  }));
+});
+
+promise.then((value) => {
+  console.log('done');
+});
