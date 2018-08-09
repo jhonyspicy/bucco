61f0e6a798c67717b4c92fb4800365aa6f39a3e1
jhonyspicy <jhonyspicy@gmail.com>
2018-08-09 15:08:08 +0900

    generate roughly

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -4,24 +4,71 @@
 
 import * as $ from "jquery";
 
-console.log(`'Allo 'Allo! Content script s`);
-
-let $hallDedamaActionForm = $('form[name=HallDedamaActionForm]');
-console.log($hallDedamaActionForm);
-
-const list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 let promise:Promise<any> = Promise.resolve();
 
-list.forEach((v, i)=> {
-  promise = promise.then(value => new Promise((resolve, reject) => {
-    setTimeout(()=> {
-      console.log(v);
-      resolve();
-    }, Math.random() * 3000);
-  }));
+start();
+
+$('#ata0 .ind .det a').first().each((i, elem) => {
+  const $elem = $(elem);
+  let href = $elem.attr('href') || '';
+  eval(href);
 });
 
 promise.then((value) => {
   console.log('done');
 });
+
+end();
+
+function start() {
+  let $form = getForm();
+
+  console.log('start');
+
+  $form.on('submit', (e) => {
+    e.preventDefault();
+
+    const data = {} as any;
+    $form.find('[name]').each((i, elem)=> {
+      const $elem = $(elem);
+      const name = $elem.attr('name') || '';
+      const value = $elem.attr('value') || '';
+
+      data[name] = value;
+    });
+
+    promise = promise.then(value => new Promise((resolve, reject) => {
+      setTimeout(()=> {
+        console.log(data);
+        resolve();
+      }, Math.random() * 3000);
+    }));
+  });
+}
+
+function end() {
+  let $form = getForm();
+
+  $form.off('submit');
+
+  console.log('end');
+}
+
+function getForm() {
+  return $('form[name=TableSelectActionForm]');
+}
+
+function openDedamaDetail(cd,num) {
+  let $form = getForm();
+  const tableSelectActionForm = document.querySelector('[name=TableSelectActionForm]');
+  tableSelectActionForm.tablenum.value=num;
+  tableSelectActionForm.forward.value = "KAKIN_TABLESELECT";
+
+  if (cd == 1){
+    tableSelectActionForm.actiontype.value="12";
+  }else{
+    tableSelectActionForm.actiontype.value="14";
+  }
+
+  $form.trigger('submit');
+}
