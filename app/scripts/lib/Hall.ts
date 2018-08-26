d4fbc213087cecd22bff4bd3d966883e77454f1a
jhonyspicy <jhonyspicy@gmail.com>
2018-08-27 02:48:32 +0900

    add class files

---
+++ app/scripts/lib/Hall.ts
@@ -0,0 +1,15 @@
+export default class Hall {
+    private _name: string;
+    private _date: string;
+
+    constructor() {
+    }
+
+    set name(name: string) {
+        this._name = name;
+    }
+
+    set date(date: string) {
+        this._date = date;
+    }
+}
---
+++ app/scripts/lib/Machine.ts
@@ -0,0 +1,21 @@
+import Graph from './Graph';
+
+export default class Machine {
+    private _name: string;
+    private _date: string;
+    number: number;
+    $elem: JQuery;
+
+    constructor(num: number, $elem: JQuery) {
+        this.number = num;
+        this.$elem = $elem;
+    }
+
+    set name(name: string) {
+        this._name = name;
+    }
+
+    set date(date: string) {
+        this._date = date;
+    }
+}
